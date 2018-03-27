d43bfab987edc56b6e50fb84e2a17e61cd9c494d
semio <prairy.long@gmail.com>
2018-03-27 16:29:36 +0800

    update script

--- etl/scripts/etl.py
+++ etl/scripts/etl.py
@@ -1,54 +1,106 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
+import os.path as osp
 import pandas as pd
-import os
-from ddf_utils.str import to_concept_id
 
 
-# configuration file paths
-source = '../source/gapdata003 version 3.xlsx'
-out_dir = '../../'
+code = 'pop'
+measure_name = 'Population'
+
+out_path = '../../'
+
+source_file = f'../source/{code}/{code}-by-gapminder.xlsx'
+mappings_file = 'https://github.com/Gapminder-Indicators/concepts/blob/master/concepts-in-gapminder-indicators.xlsx?raw=true'
+
+
+def main():
+    print('loading mapping file...')
+    mappings = pd.read_excel(mappings_file)
+
+    print('running etl...')
+    measure_id = mappings.set_index('repo_id')['concept'].loc[code]
+
+    # set concepts manually.
+    domain = [ {'concept': 'geo', 'name': 'Geo', 'domain': None, 'concept_type': 'entity_domain'},
+               {'concept': 'country', 'name': 'Country', 'domain': 'geo', 'concept_type': 'entity_set'},
+               {'concept': 'world_4region', 'name': 'four regions', 'domain': 'geo', 'concept_type': 'entity_set'},
+               {'concept': 'global', 'name': 'global', 'domain': 'geo', 'concept_type': 'entity_set'},
+               {'concept': 'domain', 'name': 'Domain', 'concept_type': 'string'} ]
+
+    domain = pd.DataFrame.from_records(domain)
+
+    domain = domain.set_index('concept')
+
+    time = pd.DataFrame.from_records([
+        {'concept': 'year', 'concept_type': 'time', 'name': 'Year'}]).set_index('concept')
+
+    strings = pd.DataFrame.from_records([
+        {'concept': 'name', 'concept_type': 'string', 'name': 'Name'}]).set_index('concept')
+
+    measure = pd.DataFrame.from_records([
+        {'concept': measure_id, 'concept_type': 'measure', 'name': measure_name}]).set_index('concept')
+
+    cdf = pd.concat([measure, strings, domain, time])
+    cdf = cdf.reset_index()
+
+    cdf['name'] = cdf['name'].fillna(cdf['concept'].map(lambda x: x.replace('_', ' ').title()))
+
+    cdf.to_csv(osp.join(out_path, 'ddf--concepts.csv'), index=False)
+
+    # country data. extract country entity and datapoints
+    country_data = pd.read_excel(source_file,
+                                 sheet_name='countries_and_territories')
+    countries = country_data[['geo', 'geo.name']].copy()
+
+    countries.columns = ['country', 'name']
+    countries.to_csv(osp.join(out_path, 'ddf--entities--geo--country.csv'), index=False)
+
+    country_df = country_data.set_index('geo').drop(['geo.name', 'indicator', 'indicator.name'], axis=1)
+    country_df = country_df.stack().reset_index()
+    country_df.columns = ['country', 'year', measure_id]
+
+    country_df.dropna(how='any').to_csv(
+        osp.join(out_path,
+                 'ddf--datapoints--{}--by--country--year.csv'.format(measure_id)), index=False)
+
+    # region data
+    region_data = pd.read_excel(source_file,
+                                sheet_name='four_regions')
+
+    region = region_data[['geo', 'geo.name']]
+    region.columns = ['world_4region', 'name']
+
+    region.to_csv(osp.join(out_path, 'ddf--entities--geo--world_4region.csv'),
+                  index=False)
+
+    region_df = region_data.set_index('geo').drop(['geo.name', 'indicator', 'indicator.name'], axis=1)
+    region_df = region_df.stack().reset_index()
+    region_df.columns = ['world_4region', 'year', measure_id]
+
+    region_df.dropna(how='any').to_csv(
+        osp.join(
+            out_path,
+            'ddf--datapoints--{}--by--world_4region--year.csv').format(measure_id), index=False)
+
+    # world data
+    world_data = pd.read_excel(source_file,
+                               sheet_name='world_total')
+
+    world = world_data[['geo', 'geo.name']]
+    world.columns = ['global', 'name']
+
+    world.to_csv(osp.join(out_path, 'ddf--entities--geo--global.csv'), index=False)
+
+    world_df = world_data.set_index('geo').drop(['geo.name', 'indicator', 'indicator.name'], axis=1)
+    world_df = world_df.stack().reset_index()
+    world_df.columns = ['global', 'year', measure_id]
+
+    world_df.dropna(how='any').to_csv(
+        osp.join(out_path,
+                 'ddf--datapoints--{}--by--global--year.csv').format(measure_id), index=False)
+
 
 if __name__ == '__main__':
-    data001 = pd.read_excel(source, sheet_name='Data')
-
-
-    # entities
-    area = data001['Area'].unique()
-    area_id = list(map(to_concept_id, area))
-    ent = pd.DataFrame([], columns=['area', 'name'])
-    ent['area'] = area_id
-    ent['name'] = area
-
-    path = os.path.join(out_dir, 'ddf--entities--area.csv')
-    ent.to_csv(path, index=False)
-
-    # datapoints
-    dp_name1 = 'Population'
-    dp_name2 = 'Population with interpolations'
-    data001_dp_1 = data001[['Area', 'Year', dp_name1]].copy()
-    data001_dp_2 = data001[['Area', 'Year', dp_name2]].copy()
-
-    data001_dp_1.columns = ['area', 'year', 'total_population']
-    data001_dp_2.columns = ['area', 'year', 'total_population_with_interpolations']
-
-    data001_dp_1['area'] = data001_dp_1['area'].map(to_concept_id)
-    data001_dp_2['area'] = data001_dp_2['area'].map(to_concept_id)
-
-    path1 = os.path.join(out_dir, 'ddf--datapoints--total_population--by--area--year.csv')
-    path2 = os.path.join(out_dir, 'ddf--datapoints--total_population_with_interpolations--by--area--year.csv')
-
-    data001_dp_1.dropna().sort_values(by=['area', 'year']).to_csv(path1, index=False)
-    data001_dp_2.dropna().sort_values(by=['area', 'year']).to_csv(path2, index=False)
-
-    # concepts
-    conc = ['total_population', 'total_population_with_interpolations', 'area', 'year', 'name']
-    cdf = pd.DataFrame([], columns=['concept', 'name', 'concept_type'])
-    cdf['concept'] = conc
-    cdf['name'] = ['Total Population', 'Total Population with interpolations', 'Area', 'Year', 'Name']
-    cdf['concept_type'] = ['measure', 'measure', 'entity_domain', 'time', 'string']
-
-    path = os.path.join(out_dir, 'ddf--concepts.csv')
-    cdf.to_csv(path, index=False)
-
+    main()
     print('Done.')
+
